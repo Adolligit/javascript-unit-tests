6bcbd463594603f7953bc4f63bf0165745e526b1
Adolligit <adelson.onod@gmail.com>
2021-11-03 23:37:24 -0300

    reescrevendo a função "vqv" pela questão semântica; fix lint; função implementada e testada

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,12 @@
       #VQV!'
 */
 
-const vqv = () => {};
+function vqv(name, age) {
+  if (name === undefined) return undefined;
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+}
 
 module.exports = vqv;
