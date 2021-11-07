4c9996e8c1b167889152fba50ddc78b8829f3c7d
Adolligit <adelson.onod@gmail.com>
2021-11-07 00:38:53 -0300

    redeclarando função e tirando chaves do if de uma linha

--- src/circle.js
+++ src/circle.js
@@ -19,16 +19,16 @@
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
 */
 
-const circle = (radius) => {
+function circle(radius) {
   const PI = 3.14;
 
-  if (!radius || typeof radius !== 'number') { return undefined; }
+  if (!radius || typeof radius !== 'number') return undefined;
 
   return {
     radius,
     area: PI * radius * radius,
     circumference: 2 * PI * radius,
   };
-};
+}
 
 module.exports = circle;
