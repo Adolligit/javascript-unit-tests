d4b0cf3514982a634dc560cf70c0e24dc0d46229
Adolligit <adelson.onod@gmail.com>
2021-11-07 17:44:53 -0300

    rescrevendo declaração da função productDetails

--- src/productDetails.js
+++ src/productDetails.js
@@ -24,19 +24,23 @@
   ]
 */
 
-const productDetails = (firstProduct, secondProduct) => [
-  {
-    name: firstProduct,
-    details: {
-      productId: `${firstProduct}123`,
+function productDetails(firstProduct, secondProduct) {
+  return [
+    {
+      name: firstProduct,
+      details: 
+      {
+        productId: `${firstProduct}123`,
+      },
     },
-  },
-  {
-    name: secondProduct,
-    details: {
-      productId: `${secondProduct}123`,
+    {
+      name: secondProduct,
+      details: 
+      {
+        productId: `${secondProduct}123`,
+      },
     },
-  },
-];
+  ];
+}
 
 module.exports = productDetails;
