70d848b7e9d69ff63d328083cded4d14c4913423
Adolligit <adelson.onod@gmail.com>
2021-11-08 12:17:19 -0300

    try 2: voltando a forma feia da declaração da função

--- src/circle.js
+++ src/circle.js
@@ -19,16 +19,16 @@
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
 */
 
-function circle(radius) {
+const circle = (radius) => {
   const PI = 3.14;
 
   if (!radius || typeof radius !== 'number') { return undefined; }
 
   return {
     radius,
-    area: PI * (radius * radius),
+    area: PI * radius * radius,
     circumference: 2 * PI * radius,
   };
-}
+};
 
 module.exports = circle;
